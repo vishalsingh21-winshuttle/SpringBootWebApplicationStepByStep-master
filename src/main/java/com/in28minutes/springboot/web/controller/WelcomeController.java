e06a08da1a90e759476bd0b0f58551d3de71c72b
vishal singh <vishal.singh@winshuttle.com>
2020-07-27 12:24:30 +0530

    commit from branch2

--- src/main/java/com/in28minutes/springboot/web/controller/WelcomeController.java
+++ src/main/java/com/in28minutes/springboot/web/controller/WelcomeController.java
@@ -7,7 +7,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-//Sample Controller
+//Sample Controller for dry run from branch2
 @Controller
 public class WelcomeController {
 
